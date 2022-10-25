41a3778a6b6933f9b19bfb735941b0375f6d318b
Randika Hasheen <rhasheen@isa.ae>
2022-10-25 12:14:19 +0530

    Initial Commit

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        
+        public string Name { get; set; }
+        [Display(Name = "Display Order")]
+        [Range(1,100,ErrorMessage ="Display order must be in range of 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
